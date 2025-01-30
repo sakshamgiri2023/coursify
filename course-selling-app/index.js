e03775ed80c9380343b1f1db170680970ff84938
sakshamgiri2023 <sakshamgiri500@gmail.com>
2025-01-30 05:31:21 +0530

    added the mongoose connection

--- course-selling-app/index.js
+++ course-selling-app/index.js
@@ -1,10 +1,13 @@
- const express = require("express");
+require('dotenv').config();
+
+const express = require("express");
 
  const { userRouter } = require("./Routes/user");
 
  const { courseRouter } = require("./Routes/course");
 
  const { adminRouter } = require("./Routes/admin");
+const { default: mongoose } = require('mongoose');
 
  const app = express();
 
@@ -12,4 +15,11 @@
  app.use("/api/v1/course", courseRouter);
  app.use("/api/v1/admin", adminRouter);
 
- app.listen(3000);
+
+ async function main(){
+
+    app.listen(3000);
+    await mongoose.connect(process.env.MONGOOSE_URL)
+    app;listen(3000); 
+ }
+
